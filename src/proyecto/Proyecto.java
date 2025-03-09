edcf7fc97fe098d1481cafe94e8fe03cbd8650d6
Ale <Ale@DESKTOP-NIGU7JM>
2025-03-09 17:55:12 -0600

    hola

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -13,8 +13,10 @@ public class Proyecto {
     /**
      * @param args the command line arguments
      */
+    
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        private String holamundo;
     }
     
 }
