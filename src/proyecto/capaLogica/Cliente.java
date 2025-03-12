3410176c9a81aa2547a9829fcdcace5b3f823662
LENOVO <LENOVO@192.168.98.24>
2025-03-12 17:06:10 -0600

    Creacion de clase cliente

---
+++ src/proyecto/capaLogica/Cliente.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.capaLogica;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Cliente {
+
+    private String cedula;
+    private String nombre;
+    //private TipoCliente tipo;
+}
