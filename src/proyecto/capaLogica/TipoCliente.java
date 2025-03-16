bccd44c976472dba82ea4fc93bc9081ab40f8ece
User <User@192.168.98.23>
2025-03-15 20:49:54 -0600

    Creacion de Enums y fima de autores

---
+++ src/proyecto/capaLogica/TipoCliente.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package proyecto.capaLogica;
+
+/**
+ *
+ * @autores 
+ * Ricardo Hernández Salas, cedula 119430725
+ * Jose Alejando Jiménez Ugalde, cedula 119400931 
+ */
+public enum TipoCliente {
+    REGULAR, 
+    CORPORATIVO; 
+    
+}
