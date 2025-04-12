1ca0ed75ebb6142f13176db95cb2959e79c1233f
LENOVO <LENOVO@192.168.98.24>
2025-04-12 13:25:55 -0600

    Creacion de estatica para el formato de fecha

---
+++ src/proyecto/capaLogica/FormatoFecha.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.capaLogica;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+/**
+ *
+* @autores 
+ * Ricardo Hernández Salas, cedula 119430725
+ * Jose Alejando Jiménez Ugalde, cedula 119400931
+ */
+public class FormatoFecha {
+    private static DateTimeFormatter formato = DateTimeFormatter.ofLocalizedPattern("dd/MM/yyyy HH:mm");
+    
+    
+    public static String formatear (LocalDateTime fechaP){
+        String fecha = formato.format(fechaP);
+        return fecha;
+    }
+    
+    
+    
+} //fin de clase
